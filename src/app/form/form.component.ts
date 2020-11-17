01a35b085ad28d1b5c3f4836b744d727234dd612
Marion Kln <marion.klein@isen.yncrea.fr>
2020-11-17 14:29:58 +0100

    màj

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Contact } from '../classes/contact';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  @Output() addRequest = new EventEmitter<Contact>();
+  contact: Contact;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.contact = new Contact(-1, '', '', '', 0, 0);
+
+  }
+
+}
