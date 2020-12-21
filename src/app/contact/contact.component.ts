0cf96c54a698870bf49e76652dfb0a8c588e710e
Lucile <64949542+lucilemzn@users.noreply.github.com>
2020-12-21 10:38:18 +0100

    Trad anglais + qqmodifs

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -11,5 +11,17 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  getLogoURL4(): string {
+
+    return 'assets/marion.jpg';
+  }
+  getLogoURL5(): string {
+
+    return 'assets/canelle.jpg';
+  }
+  getLogoURL6(): string {
+
+    return 'assets/lucile.jpeg';
+  }
 
 }
